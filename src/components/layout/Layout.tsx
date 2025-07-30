81fc4654bfbaa049f7c4a5b3aa4bc74df185cd70
vinzencor <vinzenco44a@gmail.com>
2025-07-30 15:51:16 +0530

    regional admin scrolle

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ export function Layout() {
   return (
     <div className="flex h-screen bg-gray-100">
       <Sidebar />
-      <div className="flex flex-1 flex-col overflow-hidden">
+      <div className="flex flex-1 flex-col overflow-hidden min-w-0">
         <Header />
         <main className="flex-1 overflow-y-auto p-6">
           <Outlet />
